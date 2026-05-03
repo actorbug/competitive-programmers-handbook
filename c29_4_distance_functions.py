ae479c29178917465f445a1643e45ab8ff5687ea
actorbug <gnikehcv@gmail.com>
2026-05-03 23:49:13 +0900

    return position

--- c29_4_distance_functions.py
+++ c29_4_distance_functions.py
@@ -6,13 +6,13 @@ def max_distance(p):
     minxv,minxi,maxxv,maxxi=*min(x),*max(x)
     minyv,minyi,maxyv,maxyi=*min(y),*max(y)
     if maxxv-minxv>=maxyv-minyv:
-        return (minxi,maxxi,maxxv-minxv)
+        return (p[minxi],p[maxxi],maxxv-minxv)
     else:
-        return (minyi,maxyi,maxyv-minyv)
+        return (p[minyi],p[maxyi],maxyv-minyv)
 
 class Test(unittest.TestCase):
     def test(self):
-        self.assertEqual(max_distance([0+2j,1+0j,3+3j,3+1j]),(1,2,5))
+        self.assertEqual(max_distance([0+2j,1+0j,3+3j,3+1j]),(1+0j,3+3j,5))
 
 if __name__=='__main__':
     unittest.main()

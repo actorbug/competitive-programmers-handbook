b4135378db0ef010c4fcf3b2cd60ae332a14a986
actorbug <gnikehcv@gmail.com>
2026-05-03 23:44:21 +0900

    empty input

--- c24_5_randomized_algorithms.py
+++ c24_5_randomized_algorithms.py
@@ -44,6 +44,7 @@ class Test(unittest.TestCase):
         self.assertEqual(order_statistics([8,4,3,7,6,5,2,1],3),4)
         self.assertFalse(verify(M([[1,0],[0,1]]),M([[6,8],[1,3]]),M([[8,7],[3,2]])))
         edges=[(0,1),(0,2),(0,3),(1,3),(1,4),(2,3),(3,4)]
+        self.assertEqual(coloring([]), {})
         d=coloring(edges)
         self.assertGreaterEqual(sum(d[i]!=d[j] for i,j in edges),4)
 
